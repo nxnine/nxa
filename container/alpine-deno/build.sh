347769f64027ec48e4e52ab81c837030fd4b402e
nxnine <nx.nine@gmail.com>
2021-03-17 13:33:46 -0700

    build script typo

--- container/alpine-deno/build.sh
+++ container/alpine-deno/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # build alpine base docker image
 
-docker build -f Dockerfile -t nxa/alpine-deno:1.8.1 .
+docker build -f build.ockerfile -t nxa/alpine-deno:1.8.1 .
--- container/alpine-node/slim.sh
+++ container/alpine-node/slim.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # build alpine base docker image
 
-docker build -f Dockerfile -t nxa/alpine-node:15.11-slim .
+docker build -f slim.dockerfile -t nxa/alpine-node:15.11-slim .
